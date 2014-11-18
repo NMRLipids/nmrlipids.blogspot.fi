ed5d15e6d90f6b0072d0d4c5cf573eae21723f87
ohsOllila <samuli.ollila@aalto.fi>
2014-11-18 19:04:16 +0200

    Add lipid14 files

---
+++ POPClipid14/calcORDP2.sh
@@ -0,0 +1,10 @@
+awk -v Cname="  C32" -v Hname="  H2A" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="  C32" -v Hname="  H2B" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="  C31" -v Hname="  H1A" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="  C31" -v Hname="  H1B" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="   C3" -v Hname="   HA" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="   C3" -v Hname="   HB" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="   C2" -v Hname="   HX" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="   C1" -v Hname="   HR" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+awk -v Cname="   C1" -v Hname="   HS" -f /home/ollilas1/Dropbox/codes/gro_OP.awk runPROT.gro
+
